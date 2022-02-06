dfac7d123f916fa199902a0d6794c05ee85dd7b7
Tanuj Kumar Paraste <tanuj.paraste27@gmail.com>
2022-02-06 11:44:21 +0530

    Added program to check whether a number is a prime number or not

--- Main.java
+++ Main.java
@@ -34,7 +34,21 @@ public class Main {
 
   // function to check no is prime or not
   public void checkPrimeNumber() {
+    System.out.print("Enter a number: ");
+    int number = sc.nextInt();
+    boolean isPrime = true;
+    for (int i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
 
+    if (isPrime && number != 1) {
+      System.out.println("Given number is a prime number.\n");
+    } else {
+      System.out.println("Given number is not a prime number.\n");
+    }
   }
 
   // function to print Fibonacci Series
